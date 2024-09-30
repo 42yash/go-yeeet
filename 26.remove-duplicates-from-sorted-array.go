c4a6545f57363846c87584a017520dda9e3ed86f
42yash <42yash@proton.me>
2024-09-30 21:52:08 +0530

    added 26

---
+++ 26.remove-duplicates-from-sorted-array.go
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	i := 0
+	j := 1
+	for j < len(nums) {
+		if nums[i] == nums[j] {
+			j++
+		} else {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+}
+
+// @lc code=end
+
